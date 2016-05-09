5ca73b62fc28ee8d1327c6f61010702ae0c5a131
Christoph Lange <math.semantic.web@gmail.com>
2016-05-09 20:49:45 +0200

    switched to use-package

--- .emacs.d/init/company.el
+++ .emacs.d/init/company.el
@@ -1,13 +1,14 @@
 ;; company – auto-completion
-(require 'company)
+(use-package company
+  :diminish "↹"
+  :config
+  (progn
+    (add-hook 'after-init-hook 'global-company-mode)
+    (global-set-key (kbd "M-/") 'company-complete)
+    (add-to-list 'company-backends 'company-math-symbols-unicode)))
 
-(add-hook 'after-init-hook 'global-company-mode)
-(global-set-key (kbd "M-/") 'company-complete)
-
-(add-to-list 'company-backends 'company-math-symbols-unicode)
-
-(require 'company-auctex)
-(company-auctex-init)
+(use-package company-auctex
+  :config (company-auctex-init))
 
 ;; TODO prepare better source data:
 ;; * idea for emails (mbox files):
@@ -15,20 +16,19 @@
 ;; * *.tex files:
 ;; es -r '\.tex$' | grep -a -E ':\\(svn|git|hg|Dropbox|owncloud)' | grep -a -F -v '\__MACOSX\' |  while read -r line; do file="$(cygpath "$(echo -E "$line" | tr -d '\r')")"; echo "$file"; detex "$line" > $(date +%s)-"$(basename "${file%.tex}")".txt ; done
 
-(with-eval-after-load 'company-ngram
-  ; ~/data/ngram/*.txt are used as data
-  (setq company-ngram-data-dir "~/data/ngram")
-  ; company-ngram does not support python2
-  (setq company-ngram-python "C:\\bin\\cygwin\\bin\\python3.4m")
-  (company-ngram-init)
-  ;; (add-to-list 'company-backends 'company-ngram-backend)
-  ; or use `M-x turn-on-company-ngram' and
-  ; `M-x turn-off-company-ngram' on individual buffers
-  )
-(require 'company-ngram nil t)
+(use-package company-ngram
+  :config
+  (progn
+                                        ; ~/data/ngram/*.txt are used as data
+    (setq company-ngram-data-dir "~/data/ngram")
+                                        ; company-ngram does not support python2
+    (setq company-ngram-python "C:\\bin\\cygwin\\bin\\python3.4m")
+    (company-ngram-init)
+    ;; (add-to-list 'company-backends 'company-ngram-backend)
+                                        ; or use `M-x turn-on-company-ngram' and
+                                        ; `M-x turn-off-company-ngram' on individual buffers
+    ))
 
 (add-hook 'after-init-hook 'company-statistics-mode)
 
-(require 'company-web-html)                          ; load company mode html backend
-
-(diminish 'company-mode "↹")
+(use-package company-web-html)                          ; load company mode html backend
