04c577a7f5142b687b96039dc2f6352ff61e90a5
Christoph Lange <math.semantic.web@gmail.com>
2016-06-21 14:26:29 +0200

    disabled company-ngram for now; needs further work

--- .emacs.d/init/company.el
+++ .emacs.d/init/company.el
@@ -16,18 +16,18 @@
 ;; * *.tex files:
 ;; es -r '\.tex$' | grep -a -E ':\\(svn|git|hg|Dropbox|owncloud)' | grep -a -F -v '\__MACOSX\' |  while read -r line; do file="$(cygpath "$(echo -E "$line" | tr -d '\r')")"; echo "$file"; detex "$line" > $(date +%s)-"$(basename "${file%.tex}")".txt ; done
 
-(use-package company-ngram
-  :config
-  (progn
-                                        ; ~/data/ngram/*.txt are used as data
-    (setq company-ngram-data-dir "~/data/ngram")
-                                        ; company-ngram does not support python2
-    (setq company-ngram-python "C:\\bin\\cygwin\\bin\\python3.4m")
-    (company-ngram-init)
-    ;; (add-to-list 'company-backends 'company-ngram-backend)
-                                        ; or use `M-x turn-on-company-ngram' and
-                                        ; `M-x turn-off-company-ngram' on individual buffers
-    ))
+;; (use-package company-ngram
+;;   :config
+;;   (progn
+;;                                         ; ~/data/ngram/*.txt are used as data
+;;     (setq company-ngram-data-dir "~/data/ngram")
+;;                                         ; company-ngram does not support python2
+;;     (setq company-ngram-python "C:\\bin\\cygwin\\bin\\python3.4m")
+;;     (company-ngram-init)
+;;     ;; (add-to-list 'company-backends 'company-ngram-backend)
+;;                                         ; or use `M-x turn-on-company-ngram' and
+;;                                         ; `M-x turn-off-company-ngram' on individual buffers
+;;     ))
 
 (add-hook 'after-init-hook 'company-statistics-mode)
 
