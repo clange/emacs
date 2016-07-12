aa90c90c1e3ac088cf1706585d2916adc2f6cdb5
Christoph Lange <math.semantic.web@gmail.com>
2016-07-12 23:17:13 +0200

    preparing to use our own schemas

--- .emacs.d/init/nxml.el
+++ .emacs.d/init/nxml.el
@@ -2,3 +2,4 @@
 (setq nxml-child-indent 4)
 (setq nxml-outline-child-indent 4)
 (setq nxml-sexp-element-flag t)
+(add-to-list 'rng-schema-locating-files (expand-file-name "~/.emacs.d/schemas/xslt/xslt.rnc"))
