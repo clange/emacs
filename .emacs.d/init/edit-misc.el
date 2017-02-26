0c0b48da2ad693cdbf042e968ebef394dc04b3d8
Christoph Lange <math.semantic.web@gmail.com>
2017-02-26 10:04:59 +0100

    re-disabled some dangerous commands

--- .emacs.d/init/edit-misc.el
+++ .emacs.d/init/edit-misc.el
@@ -6,10 +6,10 @@
 (diminish 'visual-line-mode "")
 
 ;; disabled functions
-(put 'downcase-region 'disabled nil)
+(put 'downcase-region 'disabled t)
 (put 'narrow-to-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
-(put 'upcase-region 'disabled nil)
+(put 'upcase-region 'disabled t)
 (put 'set-goal-column 'disabled nil)
 
 (custom-set-variables
