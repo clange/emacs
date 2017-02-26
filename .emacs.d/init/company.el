b026f20104915dd0cf649ada02bee6be9ff8339a
Christoph Lange <math.semantic.web@gmail.com>
2017-02-26 10:05:21 +0100

    giving company-ngram a chance

--- .emacs.d/init/company.el
+++ .emacs.d/init/company.el
@@ -16,18 +16,22 @@
 ;; * *.tex files:
 ;; es -r '\.tex$' | grep -a -E ':\\(svn|git|hg|Dropbox|owncloud)' | grep -a -F -v '\__MACOSX\' |  while read -r line; do file="$(cygpath "$(echo -E "$line" | tr -d '\r')")"; echo "$file"; detex "$line" > $(date +%s)-"$(basename "${file%.tex}")".txt ; done
 
-;; (use-package company-ngram
-;;   :config
-;;   (progn
-;;                                         ; ~/data/ngram/*.txt are used as data
-;;     (setq company-ngram-data-dir "~/data/ngram")
-;;                                         ; company-ngram does not support python2
-;;     (setq company-ngram-python "C:\\bin\\cygwin\\bin\\python3.4m")
-;;     (company-ngram-init)
-;;     ;; (add-to-list 'company-backends 'company-ngram-backend)
-;;                                         ; or use `M-x turn-on-company-ngram' and
-;;                                         ; `M-x turn-off-company-ngram' on individual buffers
-;;     ))
+(use-package company-ngram
+  :config
+  (progn
+                                        ; ~/data/ngram/*.txt are used as data
+    (setq company-ngram-data-dir "~/data/ngram")
+                                        ; company-ngram does not support python2
+    (setq company-ngram-python "C:\\ProgramData\\chocolatey\\bin\\python")
+    (company-ngram-init)
+    ;; (add-to-list 'company-backends 'company-ngram-backend)
+                                        ; or use `M-x turn-on-company-ngram' and
+                                        ; `M-x turn-off-company-ngram' on individual buffers
+                                        ; save the cache of candidates
+    (run-with-idle-timer 3600 t
+                         (lambda ()
+                           (company-ngram-command "save_cache")))
+    ))
 
 (add-hook 'after-init-hook 'company-statistics-mode)
 
