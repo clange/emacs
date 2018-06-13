e2ca1c96c9505583e2bbc198031a9c867925f4fd
Christoph Lange-Bever <christoph.lange-bever@iais.fraunhofer.de>
2018-06-13 10:25:47 +0200

    diary file

--- .emacs.d/init/org.el
+++ .emacs.d/init/org.el
@@ -22,6 +22,7 @@
   (define-obsolete-function-alias 'fancy-diary-display
     'diary-fancy-display "23.1")
   (setq org-agenda-include-diary t)
+  (setq org-agenda-diary-file my-org-default-file)
   (setq org-agenda-span 8)
   (setq org-agenda-start-on-weekday nil)
   (add-hook 'org-agenda-finalize-hook #'(lambda () (undo-tree-mode -1)))
