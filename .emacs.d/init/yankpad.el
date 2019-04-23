92766ad08875238a37b7d34d9fa593fa77795efe
Christoph Lange-Bever <christoph.lange-bever@iais.fraunhofer.de>
2019-04-23 08:39:46 +0200

    more functionality

--- .emacs.d/init/yankpad.el
+++ .emacs.d/init/yankpad.el
@@ -1,5 +1,8 @@
 (use-package yankpad
   :defer 10
   :config
+  (setq yankpad-file "~/svn/kwarc.info/clange/org/yankpad.org")
+  :init
   (bind-key "<f7>" 'yankpad-map)
+  (bind-key "<f12>" 'yankpad-expand)
   :ensure t)
