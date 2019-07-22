5b44996e5c37924688e6b0ffd5d9a677047efcba
Christoph Lange <math.semantic.web@gmail.com>
2019-07-22 23:21:29 +0200

    fixed color for hidestars

--- .emacs-windows.el
+++ .emacs-windows.el
@@ -305,6 +305,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "Wheat" :foreground "NavyBlue" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "outline" :family "Fira Code"))))
- '(hl-line ((t (:background "#F1DD9B")))))
+ '(default ((t (:inherit nil :stipple nil :background "Wheat" :foreground "NavyBlue" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "outline" :family "Iosevka"))))
+ '(hl-line ((t (:background "#F1DD9B"))))
+ '(org-hide ((t (:foreground "wheat")))))
 (put 'scroll-left 'disabled nil)
