37aba53bf321a5c2603bb5a43fd19601a0fe42cf
Christoph Lange <math.semantic.web@gmail.com>
2019-07-26 09:30:17 +0200

    updated to setup-cygwin; made SSH work

--- .emacs.d/init/cygwin.el
+++ .emacs.d/init/cygwin.el
@@ -1,27 +1,27 @@
 ;; Windows Cygwin shell settings
-;; TODO consider using https://www.emacswiki.org/emacs/setup-cygwin.el
+;; https://www.emacswiki.org/emacs/setup-cygwin.el
 (cond
  ((eq system-type 'windows-nt)
-  (require 'cygwin-mount)
-  (let ((CYGWINPATH "c:\\tools\\cygwin\\bin"))
-    (setenv "PATH" (concat CYGWINPATH ";" (getenv "PATH")))
-    (add-hook 'comint-output-filter-functions
-              'shell-strip-ctrl-m nil t)
-    (add-hook 'comint-output-filter-functions
-              'comint-watch-for-password-prompt nil t)
-    (setq explicit-shell-file-name (concat CYGWINPATH "\\bash.exe"))
-    ;; For subprocesses invoked via the shell
-    ;; (e.g., "shell -c command")
-    (setq shell-file-name explicit-shell-file-name))
+  (require 'setup-cygwin)
+  
+  (setenv "PATH" (concat cygwin-root-directory "\\bin;" (getenv "PATH")))
+  (setenv "TMPDIR" (getenv "TEMP"))
 
   (require 'fakecygpty)
   (fakecygpty-activate)
 
-  ;; make sure we are not using CMD's FIND
+  (defun ssh (hostname &optional flags)
+    "Start an SSH session in a shell window."
+    (interactive "MSSH to host: ")
+    (let ((buf (concat "*SSH:" hostname "*")))
+      (if (and (get-buffer buf) (get-buffer-process buf))
+          (switch-to-buffer-other-window buf)
+        (async-shell-command (concat "fakecygpty ssh " flags (when flags " ") hostname) buf))))
 
-  (add-to-list 'Info-directory-list
-               (replace-regexp-in-string "\r?\n$" ""
-                                         (shell-command-to-string "cygpath -w /usr/share/info")))
+  (defun sshx (hostname)
+    "Start an SSH session with X11 forwarding in a shell window."
+    (interactive "MSSH to host (X11): ")
+    (ssh hostname "-X"))
 
   (eval-after-load "tramp"
     '(progn
@@ -33,4 +33,16 @@
                         ((eq (car x) 'tramp-login-program) (list 'tramp-login-program "fakecygpty ssh"))
                         (t x)))
                      (assoc "sshx" tramp-methods)))
-       (setq tramp-default-method "cygssh")))))
+       (add-to-list 'tramp-methods
+                    (mapcar
+                     (lambda (x)
+                       (cond
+                        ((equal x "plink") "klink")
+                        ((eq (car x) 'tramp-login-program) (list 'tramp-login-program "klink"))
+                        (t x)))
+                     (assoc "plink" tramp-methods)))
+       (setq tramp-default-method "plink")))
+
+  (add-to-list 'Info-directory-list
+               (replace-regexp-in-string "\r?\n$" ""
+                                         (shell-command-to-string "cygpath -w /usr/share/info")))))
