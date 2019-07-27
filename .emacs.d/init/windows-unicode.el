f2bd4a6cc6d20006776c817d9ef16001a026bfad
Christoph Lange <math.semantic.web@gmail.com>
2019-07-27 19:04:04 +0200

    default to Unix newlines also on Windows

--- .emacs.d/init/windows-unicode.el
+++ .emacs.d/init/windows-unicode.el
@@ -8,7 +8,7 @@
   (set-terminal-coding-system 'utf-8)
   (set-keyboard-coding-system 'utf-8)
   ;; This from a japanese individual.  I hope it works.
-  (setq default-buffer-file-coding-system 'utf-8)
+  (setq default-buffer-file-coding-system 'utf-8-unix)
   ;; From Emacs wiki
   (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
   ;; MS Windows clipboard is UTF-16LE 
