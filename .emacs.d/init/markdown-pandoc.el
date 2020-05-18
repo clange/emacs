69801b9451fac367c4a493a4c5f9fec97c46a148
Christoph Lange <math.semantic.web@gmail.com>
2020-05-18 20:00:05 +0200

    replaced deprecated orgstruct mode by orgalist

--- .emacs.d/init/markdown-pandoc.el
+++ .emacs.d/init/markdown-pandoc.el
@@ -5,5 +5,5 @@
 (add-to-list 'auto-mode-alist '("\\.md\\|\\.pdc\\'" . markdown-mode))
 (add-hook 'markdown-mode-hook #'(lambda()
                                   (if (featurep 'pandoc) (turn-on-pandoc))))
-(add-hook 'markdown-mode-hook 'turn-on-orgstruct++)
+(add-hook 'markdown-mode-hook 'orgalist-mode)
 (add-hook 'pandoc-mode-hook 'pandoc-load-default-settings)
