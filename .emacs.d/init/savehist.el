0d85a097ba7d1b565f01b82aee456c87e8806d40
Christoph Lange <math.semantic.web@gmail.com>
2021-01-06 10:01:28 +0100

    saving more

--- .emacs.d/init/savehist.el
+++ .emacs.d/init/savehist.el
@@ -1,3 +1,5 @@
 ;; save minibuffer history
 (savehist-mode 1)
 (add-to-list 'savehist-additional-variables 'log-edit-comment-ring)
+(add-to-list 'savehist-additional-variables 'search-ring)
+(add-to-list 'savehist-additional-variables 'regexp-search-ring)
