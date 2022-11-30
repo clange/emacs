106d7804ead279bad3b9ffbc7e7942156df6506d
Christoph Lange <math.semantic.web@gmail.com>
2022-11-30 23:55:38 +0100

    filename pattern for "external editor for Outlook"

--- .emacs.d/init/post.el
+++ .emacs.d/init/post.el
@@ -1,5 +1,6 @@
 ;; E-Mails
-(setq post-mail-message "\\(mutt-[a-z0-9]+-[0-9]+-[0-9]+.*\\|vimperator-exchange\\(-xfer1\\)?\\.iais\\.fraunhofer\\.de\\(-[0-9]+\\)?\\.tmp\\|tmp_exchange\\(-xfer1\\)?\\.iais\\.fraunhofer\\.de_[_[:alnum:]]+\\|kmail.*\\.tmp\\|\\.eml\\|mutt-ator-mail\\(-[0-9]+\\)?\\)\\'")
+;; mail_.*\\.txt is for https://github.com/taiyaki32lp64/external_editor_for_outlook365
+(setq post-mail-message "\\(mail_.*\\.txt\\|mutt-[a-z0-9]+-[0-9]+-[0-9]+.*\\|vimperator-exchange\\(-xfer1\\)?\\.iais\\.fraunhofer\\.de\\(-[0-9]+\\)?\\.tmp\\|tmp_exchange\\(-xfer1\\)?\\.iais\\.fraunhofer\\.de_[_[:alnum:]]+\\|kmail.*\\.tmp\\|\\.eml\\|mutt-ator-mail\\(-[0-9]+\\)?\\)\\'")
 
 (use-package orgalist
   :ensure t)
