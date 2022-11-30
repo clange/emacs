67c1d4c11ee1939d661c57ddac25b4a54274d7ff
Christoph Lange <math.semantic.web@gmail.com>
2022-12-01 00:54:38 +0100

    adapted quoting to Outlook

--- .emacs.d/init/post.el
+++ .emacs.d/init/post.el
@@ -36,9 +36,10 @@
 ; full-quote the email; reply inline
 (defun full-quote-reply-inline ()
   (interactive)
-  (when (search-forward-regexp "^From: ")
+  ;; Thunderbird
+  (when (search-forward-regexp "^From: " nil t)
     (let ((end (point)))
-      (previous-line 2)
+      (forward-line -2)
       (delete-region (point) end))
     ; cursor is on the beginning of the sender's name/address
     (join-line t)
@@ -64,7 +65,54 @@
     ; insert quoted body at the beginning of the buffer
     (yank '(4)) ; yank below cursor
     ;; TODO (open-line 2) doesn't work
-    ))
+    )
+  ;; Outlook
+  (when (search-forward-regexp "^> -----Original Message-----$" nil t)
+    (let ((end (point)))
+      (forward-line -1)
+      (delete-region (point) end))
+    (forward-line)
+    ;; cursor is on the beginning of the header
+    (let ((begin (point)))
+      (when (search-forward-regexp "^> $" nil t)
+        ;; we assume we are in post-mode, where commenting means quoting
+        (uncomment-region begin (point)))
+      (goto-char begin))
+    (forward-line)
+    ;; We should be after the From: header; put it into a single line
+    (while (not (looking-at "^Sent: "))
+      (forward-line -1)
+      (join-line t)
+      (delete-char 1)
+      (forward-line))
+    (forward-line -1)
+    ;; delete "From: "
+    (beginning-of-line nil)
+    (let ((begin (point)))
+      (when (search-forward-regexp "^From: " nil t)
+        (delete-region begin (point))))
+    ;; Cursor is on the beginning of the sender's name/address
+    (join-line t)
+    (let ((begin (point)))
+      (when (search-forward "Sent: " nil t)
+        (delete-region begin (point))))
+    ;; Cursor is on the beginning of the date
+    (insert " on ")
+    (move-end-of-line nil)
+    (insert ":")
+    (forward-line)
+    ;; Cursor is on the beginning of the next header field – delete rest of header
+    (let ((begin (point)))
+      (when (search-forward-regexp "^[[:space:] ]*$")
+        (forward-line)
+        (delete-region begin (point))))
+    (forward-line -1)
+    (kill-region (point) (point-max))
+    (goto-char (point-min))
+    ; insert quoted body at the beginning of the buffer
+    (yank '(4)) ; yank below cursor
+    (open-line 2)
+  ))
 
 ;; alternative to full-quote-reply-inline for Outlook
 (fset 'outlook-quote-reply
@@ -81,32 +129,33 @@
             (unless (string-match-p
                      "\\(\.eml\\|mutt-ator-mail\\(-[0-9]+\\)?\\)\\'"
                      (buffer-file-name))
-              (save-excursion
-                (let ((threshold 80)
-                      ;; -1: all lines empty
-                      ;;  0: all lines less than threshold
-                      ;;  1: long lines
-                      (result 0))
-                  ;; for debugging: (goto-char (point-max)) 
-                  (post-goto-signature) 
-                  (forward-line -1)
-                  (if (bobp) ; i.e. the mail is empty
-                      (setq result -1))
-                  (while (and (not (bobp)) (= result 0))
-                    (let ((line-length (- (line-end-position) (line-beginning-position))))
-                      (cond ((> line-length threshold)
-                             (setq result 1))
-                            ((bobp)
-                             nil)
-                            (t
-                             (forward-line -1)))))
-                  (if (= result 0)
-                      (progn
-                        (setq fill-column 72)
-                        (auto-fill-mode 1))
+              (unless (string-match-p "mail_.*\\.txt" (buffer-file-name))
+                (save-excursion
+                  (let ((threshold 80)
+                        ;; -1: all lines empty
+                        ;;  0: all lines less than threshold
+                        ;;  1: long lines
+                        (result 0))
+                    ;; for debugging: (goto-char (point-max)) 
+                    (post-goto-signature) 
+                    (forward-line -1)
+                    (if (bobp) ; i.e. the mail is empty
+                        (setq result -1))
+                    (while (and (not (bobp)) (= result 0))
+                      (let ((line-length (- (line-end-position) (line-beginning-position))))
+                        (cond ((> line-length threshold)
+                               (setq result 1))
+                              ((bobp)
+                               nil)
+                              (t
+                               (forward-line -1)))))
+                    (if (= result 0)
+                        (progn
+                          (setq fill-column 72)
+                          (auto-fill-mode 1))
                                         ; we override post-uses-fill-mode
-                    (auto-fill-mode -1)
-                    (setq fill-column (max-char)))))
+                      (auto-fill-mode -1)
+                      (setq fill-column (max-char))))))
               (full-quote-reply-inline))))
 
 (setq post-signature-pattern "\\(--\\|\\(Cheers\\|Viele Grüße\\),\\|\\)")
