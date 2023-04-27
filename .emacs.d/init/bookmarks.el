965b99e3d4ce5aedd22bde93feafb9f67d208ca9
Lange-Bever <christoph.lange-bever@fit.fraunhofer.de>
2023-04-27 18:11:34 +0200

    use-package bookmark+ using quelpa

--- .emacs.d/init/bookmarks.el
+++ .emacs.d/init/bookmarks.el
@@ -1,2 +1,16 @@
-(require 'bookmark+)
+; TODO initialize quelpa like
+; https://github.com/quelpa/quelpa-use-package
+; https://github.com/quelpa/quelpa
+(use-package bookmark+
+  :quelpa (bookmark+ :fetcher wiki
+                     :files
+                     ("bookmark+.el"
+                      "bookmark+-mac.el"
+                      "bookmark+-bmu.el"
+                      "bookmark+-1.el"
+                      "bookmark+-key.el"
+                      "bookmark+-lit.el"
+                      "bookmark+-doc.el"
+                      "bookmark+-chg.el"))
+  :defer 2)
 (setq bookmark-version-control t)
