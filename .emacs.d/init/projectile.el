8666aaa843017d6ec39cd28bdfda2159d420cf2c
Christoph Lange <math.semantic.web@gmail.com>
2023-06-07 11:47:41 +0200

    readability

--- .emacs.d/init/projectile.el
+++ .emacs.d/init/projectile.el
@@ -4,10 +4,12 @@
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (setq projectile-indexing-method 'alien)
+
 (use-package helm-projectile
   :ensure t)
 (helm-projectile-on)
 (setq projectile-switch-project-action 'helm-projectile)
 (diminish 'projectile-mode "P")
+
 (use-package ag
   :ensure t)
