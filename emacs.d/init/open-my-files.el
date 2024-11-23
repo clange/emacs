1e2b74f9d77b9ad406cf16b7fa401799274e57a8
Christoph Lange <math.semantic.web@gmail.com>
2024-11-23 10:47:38 +0100

    fix order

--- emacs.d/init/open-my-files.el
+++ emacs.d/init/open-my-files.el
@@ -1,7 +1,7 @@
 (save-excursion
   (let ((large-file-warning-threshold nil))
-    (find-file-existing my-org-default-file)
-    (find-file-existing my-org-default-old-file)))
+    (find-file-existing my-org-default-old-file)
+    (find-file-existing my-org-default-file)))
 (setq initial-buffer-choice 'my-org-default-file)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
