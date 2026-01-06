e81e55d3ce229f7469b8734cf57333c2724acd59
Christoph Lange <math.semantic.web@gmail.com>
2026-01-06 23:37:39 +0100

    fixed character encoding in compilation-mode

--- emacs.d/init/windows-unicode.el
+++ emacs.d/init/windows-unicode.el
@@ -12,4 +12,19 @@
   ;; From Emacs wiki
   (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
   ;; MS Windows clipboard is UTF-16LE 
-  (set-clipboard-coding-system 'utf-16le-dos)))
+  (set-clipboard-coding-system 'utf-16le-dos)
+
+
+  (defun my-compile-utf8-advice (orig-fun &rest args)
+    "Run `compile` ensuring UTF-8 env and decoding."
+    (let ((coding-system-for-read 'utf-8)
+          (coding-system-for-write 'utf-8)
+          ;; Merge/override LANG/LC_CTYPE just for this invocation:
+          (process-environment
+           (let ((env (copy-sequence process-environment)))
+             (setenv "LANG" "en_US.UTF-8" env)
+             (setenv "LC_CTYPE" "en_US.UTF-8" env)
+             env)))
+      (apply orig-fun args)))
+
+  (advice-add 'compile :around #'my-compile-utf8-advice)))
