a751034f4acf56d3cd4698bdb8f63d5a38599cac
Christoph Lange <math.semantic.web@gmail.com>
2026-01-08 20:45:35 +0000

    switching to Scoop-installed LanguageTool

--- emacs.d/init/langtool.el
+++ emacs.d/init/langtool.el
@@ -1,7 +1,7 @@
 ;; LanguageTool
 (use-package langtool
   :ensure t)
-(setq langtool-language-tool-jar (expand-file-name "~/tools/LanguageTool/languagetool-commandline.jar"))
+(setq langtool-language-tool-jar (expand-file-name "~/scoop/apps/languagetool-java/current/languagetool-commandline.jar"))
 (global-set-key "\C-x4w" 'langtool-check-buffer)
 (global-set-key "\C-x4W" 'langtool-check-done)
 (global-set-key "\C-x4n" 'langtool-goto-next-error)
