b983c43b88e1cd9e42026cde4c761c306d4f7a0e
Christoph Lange <math.semantic.web@gmail.com>
2026-02-02 11:30:38 +0100

    orgit

--- emacs.d/init/forge.el
+++ emacs.d/init/forge.el
@@ -1,9 +1,14 @@
 ; Magit Forge
 (use-package forge
   :after magit
+  :ensure t
   :config
   (push '("gitlab.cc-asp.fraunhofer.de"        ; GITHOST
 	  "gitlab.cc-asp.fraunhofer.de/api/v4" ; APIHOST
 	  "gitlab.cc-asp.fraunhofer.de"        ; WEBHOST and INSTANCE-ID
 	  forge-gitlab-repository)             ; CLASS
 	forge-alist))
+
+(use-package orgit-forge
+  :after (org forge)
+  :ensure t)
