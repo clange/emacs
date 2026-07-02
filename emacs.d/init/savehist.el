47453033d6baa0e2d3fa47728bd3b00c4efc2384
Christoph Lange <math.semantic.web@gmail.com>
2026-07-02 10:43:26 +0200

    Helm

--- emacs.d/init/savehist.el
+++ emacs.d/init/savehist.el
@@ -3,3 +3,5 @@
 (add-to-list 'savehist-additional-variables 'log-edit-comment-ring)
 (add-to-list 'savehist-additional-variables 'search-ring)
 (add-to-list 'savehist-additional-variables 'regexp-search-ring)
+(add-to-list 'savehist-additional-variables 'helm-grep-history)
+(add-to-list 'savehist-additional-variables 'helm-occur-history)
